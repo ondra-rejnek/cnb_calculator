8bed2984e5b3404b798610e001ee2cd3a1d85d91
Ondřej Rejnek <ondrej.rejnek@gmail.com>
2024-07-15 21:39:57 +0200

    Add loader spinners

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -2,27 +2,37 @@ import ExchangeForm from "../ExchangeForm";
 import ExchangeRatesTable from "../ExchangeRatesTable";
 import { Paper, StyledH2, Wrapper } from "./styles";
 import { useFetchCNBExchangeRates } from "./hooks";
+import { ClipLoader } from "react-spinners";
+import { PRIMARY_COLOR } from "../../globalStyles";
 
 const AppWrapper = () => {
   const { data, isLoading, isFetching } = useFetchCNBExchangeRates();
 
-  if (isLoading || isFetching) {
-    return <p>Fetching data...</p>;
-  }
+  // if (isLoading || isFetching) {
+  //   return <p>Fetching data...</p>;
+  // }
 
-  if (!data) {
-    return <p>Error fetching data</p>;
-  }
+  // if (!data) {
+  //   return <p>Error fetching data</p>;
+  // }
 
   return (
     <Wrapper>
       <StyledH2>Currency Converter</StyledH2>
       <Paper>
-        <ExchangeForm exchangeRates={data} />
+        {isLoading || isFetching ? (
+          <ClipLoader color={PRIMARY_COLOR} />
+        ) : (
+          <ExchangeForm exchangeRates={data || {}} />
+        )}
       </Paper>
       <StyledH2>Current exchange rates</StyledH2>
       <Paper>
-        <ExchangeRatesTable exchangeRates={data} />
+        {isLoading || isFetching ? (
+          <ClipLoader color={PRIMARY_COLOR} />
+        ) : (
+          <ExchangeRatesTable exchangeRates={data || {}} />
+        )}
       </Paper>
     </Wrapper>
   );
